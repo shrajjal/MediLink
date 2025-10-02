5bdf437a8b7309953b0b015ef588471cc07b3244
Shrajjal Prakash <shrajjalprakash@gmail.com>
2025-10-03 00:08:13 +0530

    Update Contact.jsx

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -12,8 +12,8 @@ const Contact = () => {
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='text-gray-600 text-lg font-semibold'>Our OFFICE</p>
           <p className='text-gray-500 '>54709 Willms Station <br/>Suite 350, Washington, USA</p>
-          <p className='text-gray-500'>Tel: (415) 555‑0132 <br/>Email: greatstackdev@gmail.com</p>
-          <p className='text-gray-600 text-lg font-semibold'>Careers at PRESCRIPTO</p>
+          <p className='text-gray-500'>Tel: (415) 555‑0132 <br/>Email: shrajjal@gmail.com</p>
+          <p className='text-gray-600 text-lg font-semibold'>Careers at MediLink</p>
           <p className='text-gray-500'>Learn more about our teams and job openings.</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
         </div>
