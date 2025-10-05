3f358c1586d03965cc05c5fb17e2e3af8b2b49cf
Shrajjal Prakash <shrajjalprakash@gmail.com>
2025-10-06 01:31:44 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
   const {dToken}=useContext(DoctorContext)
 
   return aToken || dToken ?(
-    <div className='bg-[#F8F9FD]'>
+    <div className='bg-[#C8A2C8]'>
       <ToastContainer/>
       <NavBar/>
       
